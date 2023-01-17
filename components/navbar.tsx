55f41e63ec438249cb4a9b2a9f02916cbd47161b
Wyatt Walker <rwyattwalker@gmail.com>
2023-01-17 11:23:44 -0600

    Mobile Updates

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -23,42 +23,37 @@ function Navbar({hamburger, home, dark}:propTypes) {
       setOpen(true)
     }
   }
-  return(
-    <div className='w-100 m font-bold text-lg text-white bg-transparent flex justify-between z-[50000] pointer-events-auto'>
-      <Link className='hover:cursor-pointer my-auto' href={"/"}>
-        <Image src={`${dark ? '/hamburger-black.png' : '/hamburger-white.png'}`} width={50} height={50} alt="logo" />
-      </Link>
-      <div className='flex gap-8'>
-        <Link href={"/pricing"} className={`${dark && 'text-black'} my-auto cursor-pointer scroll-smooth`}>Pricing</Link>
-        <Link href={"/#form"} >
-          <button className="bg-[#EB9B2F] text-white rounded-full font-bold py-2 px-4 cursor-pointer pointer-events-none">Get Started</button>
+  return(<>
+    {!hamburger &&
+      <div className='w-100 m font-bold text-lg text-white bg-transparent flex justify-between z-[50000] pointer-events-auto'>
+        <Link className='hover:cursor-pointer my-auto' href={"/"}>
+          <Image src={`${dark ? '/hamburger-black.png' : '/hamburger-white.png'}`} width={50} height={50} alt="logo" />
         </Link>
+        <div className='flex gap-8'>
+          <Link href={"/pricing"} className={`${dark && 'text-black'} my-auto cursor-pointer scroll-smooth`}>Pricing</Link>
+          <Link href={"/#form"} >
+            <button className="bg-[#EB9B2F] text-white rounded-full font-bold py-2 px-4 cursor-pointer pointer-events-none">Get Started</button>
+          </Link>
+        </div>
       </div>
-    </div>
-  )
-  // else return(
-  //   <div className='w-full  font-bold text-lg text-black bg-white flex justify-between fixed z-[50000]'>
-  //     <Link className='hover:cursor-pointer my-auto mx-2 flex' href={"/"}>
-  //       <Image src="/evolve-black.png" width={50} height={50} alt="logo" />
-  //       <div className='my-auto text-xl font-bold'>| Order Up</div>
-  //     </Link>
-  //     <GiHamburgerMenu className={`text-3xl z-50 m-5 ${open ? 'text-blue-500' : 'text-black'}`} onClick={handleToggle}/>
-  //     {open &&
-  //       <div className='flex flex-col z-20 absolute top-0 w-full h-fit bg-white'>
-  //         {home && <button onClick={()=>{
-  //           window.scrollTo({top:viewportHeight!, behavior:"smooth"})
-  //           setOpen(false)}} className="my-auto text-blue-500 font-bold py-2 px-4 w-fit">Pricing</button>}
-  //         {!home && <Link href={"/#services"} className="my-auto text-blue-500 font-bold py-2 px-4 w-fit">Pricing</Link>}
-  //          {/* <Link href={"/careers/sales-executive"} className="my-auto text-blue-500 font-bold py-2 px-4 w-fit">Careers</Link> */}
-  //         {home && <button onClick={()=>{
-  //           window.scrollTo({top:viewportHeight! + 2000, behavior:"smooth"})
-  //           setOpen(false)}} className=" text-blue-500 font-bold py-2 px-4 w-fit">Get Started</button>}
-  //         {!home && <Link href={"/#form"} className="bg-[#EB9B2F] text-blue-500 font-bold py-2 px-4">Get Started</Link>}
-         
-  //       </div>
-  //     }
-  //   </div>
-  // )
+    }
+    {hamburger &&
+      <div className='w-full  font-bold text-lg text-black bg-white flex justify-between fixed z-[50000]'>
+        <Link className='hover:cursor-pointer my-auto mx-2 flex' href={"/"}>
+          <Image src="/hamburger-black.png" width={50} height={50} alt="logo" />
+          <div className='my-auto text-xl font-bold'>| ORDER UP</div>
+        </Link>
+        <GiHamburgerMenu className={`text-3xl z-50 m-5 ${open ? 'text-blue-500' : 'text-black'}`} onClick={handleToggle}/>
+        {open &&
+          <div className='flex flex-col z-20 absolute top-0 w-full h-fit bg-white'>
+            <Link href={"/pricing"} className="my-auto text-black font-bold py-2 px-4 w-fit">Pricing</Link>
+            <Link href={"/#form"} className=" text-black font-bold py-2 px-4">Get Started</Link>
+          
+          </div>
+        }
+      </div>
+    }
+ </> )
 }
 
 export default Navbar
--- pages/index.tsx
+++ pages/index.tsx
@@ -65,7 +65,7 @@ export default function Home() {
           <div className='flex flex-col h-full justify-center pointer-events-none '>
             <div className='flex flex-col gap-3 md:flex-col lg:my-auto main-title justify-center'>
               <div className='text-center mt-20'>ORDER UP</div>
-              <div className='text-xl mx-auto mb-10'>Digital Solutions for Your Food Truck</div>
+              <div className='text-xl mx-4 font-[500] mb-10'>Digital Solutions for Your Food Truck</div>
             </div>
             
             <div className="md:my-auto w-full mx-auto">
@@ -81,16 +81,16 @@ export default function Home() {
           </div> 
         </div>
       </main>
-      <section className='min-h-[70vh] -mt-40 pb-10 h-fit flex flex-col bg-[#5C4934]'  id="services">
+      <section className='min-h-[400px] sm:min-h-[70vh] -mt-[250px] sm:-mt-40 pb-10 h-fit flex flex-col bg-[#5C4934]'  id="services">
        
-        <div className=' h-fit mt-auto md:mx-20 lg:mx-44 flex flex-col flex-wrap xl:flex-nowrap md:flex-row lg:gap-4 justify-center text-white'> 
+        <div className=' h-fit mt-20 sm:mt-auto md:mx-20 lg:mx-44 flex flex-col flex-wrap xl:flex-nowrap md:flex-row lg:gap-4 justify-center text-white'> 
         {/* <div className='w-80 flex flex-col mt-16 text-center rounded-md mx-auto lg:mx-0'>
           <h1 className="font-bold text-3xl underline">Services</h1>
           <p className='text-lg'>We offer a full range of services to take your website or app from an idea to fruition. Wether you need one or all of the services we provide, if it&apos;s on the web, we&apos;ve got you covered. 
           </p>
         </div> */}
         <div className='md:basis-[100%] xl:hidden'></div>
-          <div className='flex flex-col max-w-md my-auto'>
+          <div className='flex flex-col max-w-md my-auto mx-2'>
             <h1 className='font-[600] text-3xl my-3'>WHAT WE DO</h1>
             <p className='font-[500]'>We take the digital hassle out of your food truck business.
               By providing you with a custom designed top tier website, 
@@ -104,7 +104,7 @@ export default function Home() {
           {/* <Service name={"Design"} /> */}
           {/* <Service name={"Develop"} /> */}
         <div className='md:basis-[100%] xl:hidden'></div>
-          <Image src={"/3DTruck.png"} width={500} height={500} alt="Food Truck Graphic"/>
+          <Image src={"/3DTruck.png"} width={500} height={500} alt="Food Truck Graphic" className='hidden sm:inline'/>
         </div>
       </section>
       <section className='min-h-[60vh] pb-10 h-fit flex flex-col bg-[#323732]'  id="services">
@@ -121,7 +121,7 @@ export default function Home() {
           {/* <Service name={"Design"} /> */}
           {/* <Service name={"Develop"} /> */}
         <div className='md:basis-[100%] xl:hidden'></div>
-               <div className='flex flex-col max-w-md my-auto'>
+               <div className='flex flex-col max-w-md my-auto mx-2'>
             <h1 className='font-bold text-3xl my-3'>RISE ABOVE THE CROWD</h1>
             <p className='font-[500]'>Showcase your unique brand and menu with a fully custom website.
             Increase visibility and reach a larger audience by taking advantage of the latest search engine optimizations. 
@@ -135,14 +135,14 @@ export default function Home() {
       </section>
       <section className='min-h-[60vh] pb-10 h-fit flex flex-col bg-[#323732]'  id="services">
        
-       <div className=' h-fit mt-auto md:mx-20 lg:mx-44 flex flex-col flex-wrap xl:flex-nowrap md:flex-row lg:gap-4 justify-center text-white'> 
+       <div className=' h-fit mt-auto md:mx-20 lg:mx-44 flex flex-col-reverse sm:flex-col flex-wrap xl:flex-nowrap md:flex-row lg:gap-4 justify-center text-white'> 
        {/* <div className='w-80 flex flex-col mt-16 text-center rounded-md mx-auto lg:mx-0'>
          <h1 className="font-bold text-3xl underline">Services</h1>
          <p className='text-lg'>We offer a full range of services to take your website or app from an idea to fruition. Wether you need one or all of the services we provide, if it&apos;s on the web, we&apos;ve got you covered. 
          </p>
        </div> */}
        <div className='md:basis-[100%] xl:hidden'></div>
-         <div className='flex flex-col max-w-md my-auto'>
+         <div className='flex flex-col max-w-md my-auto mx-2'>
            <h1 className='font-bold text-3xl my-3'>DELIVER EXPERIENCE</h1>
            <p className='font-[500]'>Deliver a flawless customer experience with online ordering via your own website. 
            Allow customers to easily place orders at the restaurant or on the go, increasing convenience and driving sales.
@@ -160,7 +160,7 @@ export default function Home() {
        </div>
      </section>
       <section className='h-fit bg-[#E8E8E8]  flex justify-center'>
-        <div className=' bg-[#323732] w-[80%] lg:w-[60%] h-fit my-5'>
+        <div className=' bg-[#323732] w-[95%] sm:w-[80%] lg:w-[60%] h-fit my-5'>
           <form id="#form" className='flex flex-col m-12 gap-4 text-black' onSubmit={handleSubmit}>
             {loading &&
             <div className='h-[391px] flex'>
