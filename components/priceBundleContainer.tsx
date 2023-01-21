35d370f3d14b1ec29f30c4e4095bbd778e1ee01f
Wyatt Walker <rwyattwalker@gmail.com>
2023-01-20 19:46:49 -0600

    Bundle

---
+++ components/priceBundleContainer.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+type props = {
+  plan: String,
+  headerText: String,
+  price: String,
+  bullets: String[],
+  classes?: String,
+  fee: String,
+  text: String,
+}
+
+function PriceBundleContainer({plan, headerText, price, bullets, classes, fee, text}:props) {
+  return (
+    <div className={`${classes && classes} flex flex-col my-auto bg-white rounded-md drop-shadow-lg min-h-[425px] w-[350px] md:w-full max-w-[725px] xl:max-w-4xl mx-auto pb-2 text-center border-[#F5F5F5] border-2 mb-10`}>
+      
+      <div className='bg-[#EB9B2F] font-bold text-lg text-white w-[100%] text-center rounded-t'>Most Popular</div> 
+      <h1 className='font-bold text-2xl mb-3 md:hidden mt-8'>{plan}</h1>
+      <div className='flex flex-col md:flex-row h-full gap-4'>
+        <div className='flex flex-col w-full md:w-1/2 my-auto'>
+          <h1 className='font-bold text-2xl mb-3 hidden md:inline'>{plan}</h1>
+          <h2 className='mt-2 mb-3'><span className='font-bold text-3xl'>{`${price}`}</span>&nbsp;/month</h2>
+          <p className='mb-2'><span className='font-bold text-lg'>{`${fee}`}</span>&nbsp; set up fee</p>
+          <p className='my-4 mx-2'>{text}</p>
+        
+          <button className='py-3 px-4 rounded-full bg-[#97BBAF] my-3 font-semibold mx-4'>Get Started</button>
+        </div>
+        <div className='separator h-[80%] w-[80%] md:w-[1px] flex flex-col md:flex-row my-auto mx-auto md:mx-0'></div>
+        <div className='flex flex-col mx-auto'>
+            {/* <div>Our best deal for the digital friendly food truck, includes:</div> */}
+            <ul className='list-none my-auto '>
+            {bullets.map((e,i) => {
+              return(
+              <div className='flex my-1 gap-2 font-semibold' key={i}>
+                <svg className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
+                </svg>
+                <li>{e}</li>
+              </div>
+              )
+            })}
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default PriceBundleContainer
--- pages/pricing.tsx
+++ pages/pricing.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../components/navbar'
 import PricingContainer from '../components/pricingContainer'
+import PriceBundleContainer from '../components/priceBundleContainer'
 
 export default function Pricing() {
   return (
@@ -9,7 +10,7 @@ export default function Pricing() {
         <Navbar hamburger={true} dark/>
       </div>
    <section className='w-[100vw] max-w-[100vw] overflow-x-hidden min-h-[100vh] text-center  bg-[#E8E8E8] flex'>
-    <div className='flex flex-col justify-center min-h-[100vh] gap-4 mx-auto mt-12 2xl:mt-0'>
+    <div className='flex flex-col justify-center min-h-[100vh] gap-4 mx-auto mt-12'>
       <div className='hidden md:inline'>
         <Navbar hamburger={false} dark/>
       </div>
@@ -18,11 +19,13 @@ export default function Pricing() {
         <p className=' lg:max-w-3xl mx-2 sm:mx-auto text-center'>Lorem ipsum dolor sit amett consectetud adipisicing elit. Voluptas voluptatibus, veritatis voluptates iure nisi cumque expedita veniam fuga eius deleniti autem ullam sequi nostrum error repellat dolorem vel porro delectus.</p>
       </div>
         <div className='grid grid-cols-1 md:grid-cols-2 md:max-w-[725px] 2xl:max-w-none 2xl:grid-cols-4 justify-center gap-5 mt-5 mb-5 mx-auto' >
-          <PricingContainer text={"A mobile-optimized, preset website with seamless content management and SEO best practices built in."} classes={"2xl:mt-10"} plan={"Digital Essentials"} headerText={"Sign up for the basic plan."} price={"$119"} fee={"$900"} bullets={["Custom Website", "Real Time Location Display", "SEO Optimization", "Content Management System"]} />
-          <PricingContainer text={"Digital storefront custom-designed to elevate your brand. Includes built-in marketing and commerce tools."} classes={"2xl:-mt-1"} fee={"$1200"} plan={"Digital Plus"} headerText={"Sign up for the basic plan."} price={"$150"} bullets={["Custom Website", "Real Time Location Tracking"]} />
-          <PricingContainer text={"Sell and manage to-go orders directly on your website."} fee={"$799"} plan={"Mobile Ordering"} classes={"2xl:-mt-1"} headerText={"Sign up for the basic plan."} price={"$150"} bullets={["Custom Website", "Real Time Location Tracking"]} />
-          <PricingContainer text={"From patio buyouts to weddings, birthdays, and more, our platform makes it easy for diners to book"} fee={"$100"} classes={"2xl:mt-10"} plan={"Event Booking"} headerText={"Sign up for the basic plan."} price={"$150"} bullets={["Custom Website", "Real Time Location Tracking"]} />
+          <PricingContainer text={"A mobile-optimized, preset website with seamless content management and SEO best practices built in."} classes={"2xl:mt-10"} plan={"Basic Web"} headerText={"Sign up for the basic plan."} price={"$99"} fee={"$999"} bullets={["Custom Website", "Real Time Location Display", "SEO Optimization", "Content Management System"]} />
+          <PricingContainer text={"Digital storefront custom-designed to elevate your brand. Includes built-in marketing and commerce tools."} classes={"2xl:-mt-1"} fee={"$1199"} plan={"Premium Web"} headerText={"Sign up for the basic plan."} price={"$199"} bullets={["Custom Website", "Real Time Location Tracking"]} />
+          <PricingContainer text={"Sell and manage to-go orders directly on your website."} fee={"$499"} plan={"Mobile Ordering"} classes={"2xl:-mt-1"} headerText={"Sign up for the basic plan."} price={"$69"} bullets={["Custom Website", "Real Time Location Tracking"]} />
+          <PricingContainer text={"From patio buyouts to weddings, birthdays, and more, our platform makes it easy for diners to book"} fee={"$199"} classes={"2xl:mt-10"} plan={"Event Booking"} headerText={"Sign up for the basic plan."} price={"$49"} bullets={["Custom Website", "Real Time Location Tracking"]} />
         </div>
+          <div className='font-bold text-4xl'>Bundle & Save!</div>
+          <PriceBundleContainer text={"Our best deal for the 21st Century Food Truck! Includes everything from the Premium Web, Mobile Ordering, and Event Booking Packages, all for one low price."} fee={"$1499"} classes={"2xl:mt-10"} plan={"Order Up Bundle!"} headerText={"Sign up for the basic plan."} price={"$249"} bullets={["Custom Website", "SEO Optimization", "Real Time Location Tracking", "Accept and Manage Orders Online", "Easily Update Menu", "Yearly Site Redesign"]} />
      </div>
     </section>
     </>
