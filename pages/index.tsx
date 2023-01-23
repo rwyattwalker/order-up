93aaa8ce1fe19da8a9cf814519a68dc7e437aa95
Wyatt Walker <rwyattwalker@gmail.com>
2023-01-22 22:08:55 -0600

    Contact

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,45 +6,13 @@ import Service from '../components/service'
 import { use, useEffect, useState } from 'react'
 import CircularProgress from '@mui/material/CircularProgress';
 import ClientOnly from '../components/ClientOnly'
+import Link from 'next/link'
 
 type formResType = {
   success: boolean
 }
 
 export default function Home() {
-  const [formState, setFormState] = useState({
-    fname:'',
-    lname: '',
-    cname:'',
-    country:'',
-    email:'',
-    phone:'',
-    preferredContact:'',
-    message:''
-  })
-  
-  const [formRes, setFormRes] = useState<formResType|null>(null);
-  const [loading, setLoading] = useState<boolean>(false);
-  const handleChange = (e:any, value:any) => {
-    let current:any = formState;
-    current[value] = e.target.value;
-    setFormState(current);
-    console.log(formState);
-  }
-  async function handleSubmit(e:any) {
-    e.preventDefault();
-    setLoading(true)
-    const response = await fetch("/api/form", {
-      method: 'POST',
-      headers:{
-        'Content-Type': 'application/json'
-      },
-      body:JSON.stringify(formState)
-    });
-    const json = await response.json();
-    setLoading(false)
-    setFormRes(json)
-  }
   return (
     <div className=''>
       <Head>
@@ -165,59 +133,11 @@ export default function Home() {
        </div>
      </section>
       <section className='h-fit bg-[#E8E8E8]  flex justify-center'>
-        <div className=' bg-[#323732] w-[95%] sm:w-[80%] lg:w-[60%] h-fit my-5'>
-          <form id="#form" className='flex flex-col m-12 gap-4 text-black' onSubmit={handleSubmit}>
-            {loading &&
-            <div className='h-[391px] flex'>
-              <div className='flex my-auto mx-auto'>
-                <CircularProgress className=''/>
-              </div>
-            </div>
-           
-            }
-            {formRes && formRes.success == true && 
-            <div className='h-[391px] flex text-center md:text-left text-white'>
-             <div className='flex-col my-auto'>
-                <h1 className='mx-auto md:ml-0 w-fit font-bold text-5xl mb-2'>Order Up!</h1>
-                <h2 className='my-auto mx-auto w-fit font-bold relative mb-2'>
-                We have received your submission. We will reach out to you within 24 hours via your preferred contact method. We look forward to working with you.
-              </h2>
-              <button type="button" className='py-2 px-4 bg-[#97BBAF] rounded-sm font-bold text-white' onClick={() =>setFormRes(null)}>Submit another message</button>
-              </div>
-            </div>
-          }
-          {formRes == null && !loading &&
-          <>
-            <h1 className='font-bold text-3xl text-white'>Ready to Get Started?</h1>
-            <div className='flex flex-col lg:flex-row gap-4'>
-              <input type="text" id="fname" onChange={(e)=>handleChange(e,"fname")} name="fname" placeholder=" First Name" className="lg:w-1/3 h-10 rounded-sm"/>
-              <input type="text" id="lname" onChange={(e)=>handleChange(e,"lname")} name="lname" placeholder=" Last Name" className="lg:w-1/3 h-10 rounded-sm"/>
-              <input type="text" id="country" onChange={(e)=>handleChange(e,"country")} name="country" placeholder=" Country" className="lg:w-1/3 h-10 rounded-sm"/>
-            </div>
-            <div className='flex flex-col lg:flex-row gap-4'>
-              <input type="text" id="cname" onChange={(e)=>handleChange(e,"cname")} name="cname" placeholder=" Company Name" className="lg:w-1/3  h-10 rounded-sm"/>
-              <input type="text" id="email" onChange={(e)=>handleChange(e,"email")} name="email" placeholder=" Email" className="lg:w-1/3 h-10 rounded-sm"/>
-              <input type="text" id="phone" onChange={(e)=>handleChange(e,"phone")} name="phone" placeholder=" Phone" className="lg:w-1/3 h-10 rounded-sm"/>
-            </div>
-            
-            <textarea style={{color:"black !important"}} rows={6} cols={3} id="message" onChange={(e)=>handleChange(e,"message")} name="message" className='rounded-sm p-2' placeholder="Message" />
-            <div className='flex flex-col sm:flex-row text-white'>
-              Preferred Contact Method:&nbsp;
-              <div className='flex'>
-                <input type="radio" id="via_email" onChange={(e)=>handleChange(e,"preferredContact")} value="email" name="fav_contact" />
-                <label htmlFor="via_email" className='mx-1'>Email</label>
-
-                <input type="radio" id="via_phone" onChange={(e)=>handleChange(e,"preferredContact")} value="phone" name="fav_contact" />
-                <label htmlFor="via_phone" className='mx-1'>Phone</label>
-              </div>
-             
-            </div>
-            <button  type="submit" className='py-2 px-4 bg-[#97BBAF] lg:w-44 text-white font-bold rounded-sm'>Submit</button>
-            </>
-          }
-
-          </form>
-          <div id="form" className='w-full bg-transparent'></div>
+        <div className=' bg-[#323732] w-[95%] sm:w-[80%] lg:w-[60%] h-44 flex flex-col sm:flex-row justify-between rounded-full my-5'>
+         <h1 className='my-auto text-white text-5xl font-semibold mx-auto sm:ml-10 sm:mr-0'>Questions?</h1>
+         <Link href={'/contact'} className='my-auto'>
+            <h1 className='my-auto text-white text-4xl font-semibold mx-auto sm:mr-10 sm:ml-0 bg-[#97BBAF] py-4 px-5 w-fit rounded-full'>Contact Us</h1>
+         </Link>
         </div>
       </section>
     </div>
