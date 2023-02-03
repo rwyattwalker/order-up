90c1c64e685a3db0ad40999f9bbb013a708137c3
Wyatt Walker <rwyattwalker@gmail.com>
2023-02-03 13:20:47 -0600

    Client Only Nav

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,21 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Navbar from '../components/navbar'
 import { ShoppingCartProvider } from '../context/ShoppingCartContext'
+import ClientOnly from '../components/ClientOnly'
 
 export default function App({ Component, pageProps }: AppProps) {
   return(
   <ShoppingCartProvider>
-    <div className='hidden sm:inline'>
-      <Navbar hamburger={false} getStarted dark shoppingCart/>
-    </div>
-    <div className='sm:hidden'>
+    <ClientOnly>
+       <div className='hidden sm:inline'>
+        <Navbar hamburger={false} getStarted dark shoppingCart/>
+      </div>
+    </ClientOnly>
+   <ClientOnly>
+     <div className='sm:hidden'>
       <Navbar hamburger={true} getStarted dark />
     </div>
+   </ClientOnly>
     <Component {...pageProps} />
   </ShoppingCartProvider>
   )
