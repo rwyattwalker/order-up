54b2af3c28054878380a5fa36f9c5bc1d1ab5852
Wyatt Walker <rwyattwalker@gmail.com>
2023-02-03 19:30:35 -0600

    Payment Redirect

--- components/CheckoutForm.jsx
+++ components/CheckoutForm.jsx
@@ -120,7 +120,7 @@ export default function CheckoutForm({clientSecret}) {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: "http://localhost:3000",
+        return_url: "https://getorderup.com",
       },
     });
 
