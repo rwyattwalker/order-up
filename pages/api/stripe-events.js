809ecba8e078f20e49cee63dc3723d771e52cf3e
Wyatt Walker <rwyattwalker@gmail.com>
2023-02-03 21:12:13 -0600

    Update API Version

--- pages/api/stripe-events.js
+++ pages/api/stripe-events.js
@@ -2,7 +2,7 @@ import { buffer } from "micro";
 import Stripe from "stripe";
 
 const stripe = new Stripe("sk_test_51MSWxUFWbP8S10HmW4UE1p1kLW0ENFES63lJzm5tHQrBMloejWuL2N21GqMXaundLLOqQ68GmF3WQwxVlwkVEvjK005df3CpUQ", {
-  apiVersion: "2020-08-27",
+  apiVersion: "2022-11-15",
 });
 const webhookSecret = "whsec_DaACbTV2YOJdH282s3J2DgdkRv0WSxFy";
 
