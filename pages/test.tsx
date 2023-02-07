79db0faf98e60427756f9638dc9010f368bf829c
Wyatt Walker <rwyattwalker@gmail.com>
2023-02-07 17:42:14 -0600

    HTML Email

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,72 +1,19 @@
 import React from 'react'
-import { useAcceptJs } from 'react-acceptjs';
-
-const authData = {
-  apiLoginID: 'YOUR AUTHORIZE.NET API LOGIN ID',
-  clientKey: 'YOUR AUTHORIZE.NET PUBLIC CLIENT KEY',
-};
-
-type BasicCardInfo = {
-  cardNumber: string;
-  cardCode: string;
-  month: string;
-  year: string;
-};
 
 const App = () => {
-  const { dispatchData, loading, error } = useAcceptJs({ authData });
-  const [cardData, setCardData] = React.useState<BasicCardInfo>({
-    cardNumber: '',
-    month: '',
-    year: '',
-    cardCode: '',
-  });
-
-  const handleSubmit = async (event:any) => {
-    event.preventDefault();
-    // Dispatch CC data to Authorize.net and receive payment nonce for use on your server
-    const response = await dispatchData({ cardData });
-    console.log('Received response:', response);
-  };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        name="cardNumber"
-        value={cardData.cardNumber}
-        onChange={(event) =>
-          setCardData({ ...cardData, cardNumber: event.target.value })
-        }
-      />
-      <input
-        type="text"
-        name="month"
-        value={cardData.month}
-        onChange={(event) =>
-          setCardData({ ...cardData, month: event.target.value })
-        }
-      />
-      <input
-        type="text"
-        name="year"
-        value={cardData.year}
-        onChange={(event) =>
-          setCardData({ ...cardData, year: event.target.value })
-        }
-      />
-      <input
-        type="text"
-        name="cardCode"
-        value={cardData.cardCode}
-        onChange={(event) =>
-          setCardData({ ...cardData, cardCode: event.target.value })
-        }
-      />
-      <button type="submit" disabled={loading || error}>
-        Pay
-      </button>
-    </form>
+  <div className='mt-20'>
+    <div style={{width:'full', maxHeight:'400px', display:'flex', gap:"50px", justifyContent:"center", backgroundColor:'#e1e1e1'}}>
+      <h1 style={{marginTop:'auto', marginBottom:"auto", fontSize:"44px", fontWeight:"800"}}>Order Up!</h1>
+      <img src='/food-web-2.png' alt='food truck image' style={{objectFit:'contain', width:500}} />
+    </div>
+    <div style={{marginTop:"40px"}}>
+      <h1 style={{textAlign:"center", fontSize:"32px", fontWeight:"800",borderTop:"2px solid grey"}}>Get ready to have the coolest food truck in town!</h1>
+      <h1 style={{textAlign:"left", fontSize:"22px", fontWeight:"500" }}>Here&apos;s what to expect going forward</h1>
+    </div> 
+   
+  </div> 
   );
 };
 
