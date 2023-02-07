2c7674cefe4d5eceadcf70e3e7ec54ecf5d39da3
Wyatt Walker <rwyattwalker@gmail.com>
2023-02-06 19:10:54 -0600

    BG White

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="crossorigin" />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;600&display=swap" rel="stylesheet" />
+        <meta name="theme-color" content="#ffffff"></meta>
       </Head>
       <body className='scrollbar-hide bg-white'>
         <Main />
